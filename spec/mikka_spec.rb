dfdb0110897c7f0ec5312f1791e75a3f5b9a2a23
mluu <mluu@lancope.com>
2014-07-17 13:38:08 -0700

    clean up spec
    utilizie rspec memoization

--- spec/mikka_spec.rb
+++ spec/mikka_spec.rb
@@ -1,59 +1,49 @@
 require_relative 'spec_helper'
 
-module Mikka
-  class TestActor < Mikka::Actor
-    def receive(msg)
-      sender << msg
+describe Mikka do
+  let(:test_actor) do
+    Class.new(Mikka::Actor) do
+      def receive(msg)
+        sender << msg
+      end
     end
   end
 
+  let(:system) { Mikka.create_actor_system('testsystem') }
+  after { system.shutdown }
+
   describe 'actor creation' do
-    before do
-      @system = Mikka.create_actor_system('testsystem')
-    end
-
-    after do
-      @system.shutdown
-    end
-
     it 'creates an actor from a class' do
-      actor_props = Props[TestActor]
-      actor = @system.actor_of(actor_props, 'some_actor')
-      actor.should be_a(ActorRef)
+      actor_props = Mikka::Props[test_actor]
+      actor = system.actor_of(actor_props, 'some_actor')
+      actor.should be_a(Mikka::ActorRef)
     end
 
     it 'creates an actor from a factory proc' do
-      actor_props = Props[:creator => proc { TestActor.new }]
-      actor = @system.actor_of(actor_props, 'some_actor')
-      actor.should be_a(ActorRef)
+      actor_props = Mikka::Props[:creator => proc { test_actor.new }]
+      actor = system.actor_of(actor_props, 'some_actor')
+      actor.should be_a(Mikka::ActorRef)
     end
 
     it 'creates an actor from a factory block' do
-      actor_props = Props.create { TestActor.new }
-      actor = @system.actor_of(actor_props, 'some_actor')
-      actor.should be_a(ActorRef)
+      actor_props = Mikka::Props.create { test_actor.new }
+      actor = system.actor_of(actor_props, 'some_actor')
+      actor.should be_a(Mikka::ActorRef)
     end
 
-    it 'creates an actor from a factory block passed to the Props function' do
-      actor_props = Useful.Props { TestActor.new }
-      actor = @system.actor_of(actor_props, 'some_actor')
-      actor.should be_a(ActorRef)
+    it 'creates an actor from a factory block passed to the Mikka::Props function' do
+      actor_props = Mikka::Useful.Props { test_actor.new }
+      actor = system.actor_of(actor_props, 'some_actor')
+      actor.should be_a(Mikka::ActorRef)
     end
   end
 
   describe 'message sending' do
-    before do
-      @system = Mikka.create_actor_system('testsystem')
-      @actor = @system.actor_of(Props[TestActor])
-    end
-
-    after do
-      @system.shutdown
-    end
+    let(:actor) { system.actor_of(Mikka::Props[test_actor]) }
 
     describe '#tell/#<<' do
       it 'sends a message to an actor' do
-        @actor << 'hello'
+        actor << 'hello'
       end
     end
 
@@ -76,11 +66,11 @@ module Mikka
           end
         end
       end
-      let(:reverser) { @system.actor_of(Props[reverser_actor], 'reverser_actor') }
+      let(:reverser) { system.actor_of(Mikka::Props[reverser_actor], 'reverser_actor') }
 
       it "sends to another actor" do
         reverser_actor.should_receive(:reverse_it).with('pipe').and_return('epipe')
-        @actor.ask('pipe').pipe_to(reverser)
+        actor.ask('pipe').pipe_to(reverser)
         sleep 0.2
       end
     end
